509df003fde220f936c11c7706a7c88867252ab5
Deepika Chauhan <137873608+deepikachauhan8988@users.noreply.github.com>
2024-08-05 13:39:17 +0530

    Add files via upload

---
+++ update_profile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid p-5 bg-primary text-white text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <form action="action_page.php">
+
+    <label for="fname"> Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">email</label>
+    <input type="text" id="email" name="emailid" placeholder="Your email..">
+
+    <label for="fname"> mobile no</label>
+    <input type="text" id="phone" name="firstname" placeholder="Your phone num....">
+
+    <label for="subject">password</label>
+    <input type ="password" id="pass" name="password" placeholder="your password.." ></input>
+
+    <button type="submit" value="Submit"name="submit">
+
+</body>
+</html>
